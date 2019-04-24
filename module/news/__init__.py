3dc92c5cc60d11364d06ecc1f1d694b46500fd86
Shashank Khandelwal <Shashank.11609001@lpu.in>
2019-04-24 22:47:43 +0530

    Add files via upload
    Updated methods for news by query

--- module/news/__init__.py
+++ module/news/__init__.py
@@ -1,2 +1,4 @@
 from .news import top_headlines
 from .news import top_headlines_link 
+from .news import get_news
+from .news import query_headlines_link
--- module/news/news.py
+++ module/news/news.py
@@ -1,5 +1,7 @@
 from newsapi import NewsApiClient 
 news = NewsApiClient(api_key='6bb8e26abfc14f30bdf9a25ff6a893bf')
+
+# Top News
 top_news = news.get_top_headlines(language='en',page_size=10)
 
 def top_headlines():
@@ -19,3 +21,30 @@ def top_headlines_link():
 		top_headlines_link.append(top_news['articles'][i]['url'])
 	return top_headlines_link
 
+# News By Query
+def get_news(query):
+	query_news = news.get_everything(q=query,language='en',page_size=20)
+	query_headlines=[]
+	for i in range(len(query_news['articles'])):
+		#111
+		title = query_news['articles'][i]['title']
+		if(len(title)>105):
+			query_headlines.append(title[:105]+'. . .')
+		else:
+			query_headlines.append(title)
+	return query_headlines
+
+def query_headlines_link(query):
+	query_news = news.get_everything(q=query,language='en',page_size=20)
+	query_headlines_link=[]
+	for i in range(len(query_news['articles'])):
+		query_headlines_link.append(query_news['articles'][i]['url'])
+	return query_headlines_link
+'''
+# Tester
+x = get_news('srilanka')
+[print(i) for i in x]
+
+x = query_headlines_link('srilanka')
+[print(i) for i in x]
+'''
